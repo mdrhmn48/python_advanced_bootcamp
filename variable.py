6fb3994fe2665a80d5da24fb511e102f823355d0
MD Rahman <99766671+mdrhmn48@users.noreply.github.com>
2023-04-06 23:47:20 -0400

    day 2

---
+++ variable.py
@@ -0,0 +1,13 @@
+variable = "value"
+age = 30
+print(age)
+
+print(variable)
+
+course = "Python"
+print("You are taking a " + course + " course")
+
+my_number = 1 + 3.5
+print(my_number)
+print(type(my_number))
+
